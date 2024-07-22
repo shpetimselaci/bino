d5f6ec8b61785275f7b26cfe7b5d4a99b6ad4f10
Shpetim Selaci <shselaci3@gmail.com>
2024-07-22 09:28:35 +0200

    Fix tailwind not loading

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -4,9 +4,8 @@ const config = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    './src/app/**/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
 	],
   prefix: "",
   theme: {
