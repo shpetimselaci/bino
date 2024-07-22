604d01e781c48475b642142082f525d5e039c49e
Shpetim Selaci <shselaci3@gmail.com>
2024-07-22 09:14:59 +0200

    Add shadcn lib

--- app/src/components/Button/Button.stories.tsx
+++ app/src/components/Button/Button.stories.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import Button from "./Button";
+import {Button} from "./Button";
 
 export default {
   title: "Components/Button",
   component: Button,
 };
 
-export const Primary = () => <Button variant="primary">Primary</Button>;
+export const Primary = () => <Button variant="default">Primary</Button>;
 export const Secondary = () => <Button variant="secondary">Secondary</Button>;
-export const Success = () => <Button variant="success">Success</Button>;
-export const Danger = () => <Button variant="danger">Danger</Button>;
+export const Success = () => <Button variant="destructive">Success</Button>;
+export const Danger = () => <Button variant="link">Danger</Button>;
 
 export const Template = (args: any) => <Button {...args} />;
